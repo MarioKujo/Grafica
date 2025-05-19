0271099a8c21595b965b269ba99088b0ff175648
MaKu <andrmatgonros@gmail.com>
2025-05-19 20:51:28 +0200

    Object ahora aplica internamente las texturas
    Y diferencia entre objetos con transparencia, con heightmap o normales

--- Code/Headers/Object.hpp
+++ Code/Headers/Object.hpp
@@ -7,18 +7,33 @@ namespace udit
         Object(Mesh* mesh, ShaderProgram* shader);
 
         void render(const glm::mat4& view, const glm::mat4& projection);
+
         void setPosition(const glm::vec3& pos);
         void setRotation(const glm::vec3& rot);
         void setScale(const glm::vec3& scl);
+
+        void setTextureID(GLuint texture);
+        void setHeightmapTextureID(GLuint texture);
+
         ShaderProgram* getShader() const;
+
         Mesh* getMesh() const;
+
+
     private:
         Mesh* mesh;
         ShaderProgram* shader;
 
+        GLuint textureID;
+        GLuint heightmapID;
+
+        GLint heightmapLoc;
+        GLint transparencyLoc;
+
         glm::vec3 position;
         glm::vec3 rotation;
         glm::vec3 scale;
+
 
         glm::mat4 computeModelMatrix() const;
     };
--- Code/Sources/Object.cpp
+++ Code/Sources/Object.cpp
@@ -3,21 +3,69 @@
 namespace udit
 {
     Object::Object(Mesh* m, ShaderProgram* s)
-        : mesh(m), shader(s), position(0), rotation(0), scale(1)
-    {}
+        : mesh(m), shader(s), position(0), rotation(0), scale(1), textureID(0), heightmapID(0)
+    {
+        heightmapLoc = glGetUniformLocation(shader->id(), "heightmap");
+        transparencyLoc = glGetUniformLocation(shader->id(), "transparency");
+    }
 
     void Object::render(const glm::mat4& view, const glm::mat4& projection)
     {
         shader->use();
+        glActiveTexture(GL_TEXTURE0);
+        if (heightmapLoc != -1)
+        {
+            shader->setInt("heightmap", 0);
+            glBindTexture(GL_TEXTURE_2D, heightmapID);
+            glActiveTexture(GL_TEXTURE1);
+            shader->setInt("textureSampler", 1);
+            glBindTexture(GL_TEXTURE_2D, textureID);
+
+            shader->setFloat("height_scale", 25.f);
+        }
+        else if (transparencyLoc != -1)
+        {
+            shader->setInt("textureSampler", 0);
+
+            glBindTexture(GL_TEXTURE_2D, textureID);
+
+            glDepthMask(GL_FALSE);
+            glEnable(GL_BLEND);
+            glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+            shader->setFloat("transparency", 0.5f);
+        }
+        else
+        {
+            shader->setInt("textureSampler", 0);
+            glBindTexture(GL_TEXTURE_2D, textureID);
+        }
+
         glm::mat4 model = computeModelMatrix();
         shader->setMat4("model_view_matrix", (view * model));
         shader->setMat4("projection_matrix", projection);
         mesh->render();
+
+        if (transparencyLoc != -1)
+        {
+            glDisable(GL_BLEND);
+            glDepthMask(GL_TRUE);
+        }
     }
 
     void Object::setPosition(const glm::vec3& pos) { position = pos; }
     void Object::setRotation(const glm::vec3& rot) { rotation = rot; }
     void Object::setScale(const glm::vec3& scl) { scale = scl; }
+
+    void Object::setTextureID(GLuint texture)
+    {
+        textureID = texture;
+    }
+
+    void Object::setHeightmapTextureID(GLuint texture)
+    {
+        heightmapID = texture;
+    }
 
     ShaderProgram* Object::getShader() const
     {
