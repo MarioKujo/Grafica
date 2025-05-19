7dfaac3ffada77d184e13e3d722e779025dfa0e9
MaKu <andrmatgonros@gmail.com>
2025-05-20 00:07:20 +0200

    Clase Skybox comentada y refactorizada

--- Code/Headers/Skybox.hpp
+++ Code/Headers/Skybox.hpp
@@ -12,24 +12,51 @@
 
 namespace udit
 {
-      class Skybox
-      {
-      public:
+    /**
+     * @class Skybox
+     * @brief Clase que representa un skybox para el entorno 3D.
+     *
+     * Maneja la carga, asignación y renderizado de la textura del skybox.
+     */
+    class Skybox
+    {
+    public:
 
-          Skybox(MeshData mesh, ShaderProgram* shader);
+        /**
+         * @brief Constructor que inicializa el skybox con los datos de la malla y el shader.
+         * @param mesh Datos de la malla que representa el skybox.
+         * @param shader Puntero al shader que se usará para renderizar el skybox.
+         */
+        Skybox(MeshData mesh, ShaderProgram* shader);
 
-          ~Skybox();
+        /**
+         * @brief Destructor que libera la textura del skybox.
+         */
+        ~Skybox();
 
-          void setTexture(GLuint textureID);
+        /**
+         * @brief Establece la textura del skybox.
+         * @param textureID Identificador de la textura de cubemap.
+         */
+        void setTexture(GLuint textureID);
 
-          GLuint getTextureID();
+        /**
+         * @brief Obtiene el identificador de la textura del skybox.
+         * @return El ID de la textura.
+         */
+        GLuint getTextureID();
 
-          void render(const glm::mat4& view, const glm::mat4& projection);
+        /**
+         * @brief Renderiza el skybox usando las matrices de vista y proyección proporcionadas.
+         * @param view Matriz de vista.
+         * @param projection Matriz de proyección.
+         */
+        void render(const glm::mat4& view, const glm::mat4& projection);
 
-      private:
-          Mesh mesh;
-          ShaderProgram* shader;
+    private:
+        Mesh mesh;                  ///< Malla que representa el skybox.
+        ShaderProgram* shader;      ///< Shader usado para renderizar el skybox.
 
-          GLuint textureID;  ///< Identificador de la textura del skybox.
-      };
+        GLuint textureID;           ///< Identificador de la textura del skybox.
+    };
 }
--- Code/Sources/Skybox.cpp
+++ Code/Sources/Skybox.cpp
@@ -2,40 +2,55 @@
 
 namespace udit
 {
-
-    Skybox::Skybox(MeshData m, ShaderProgram* s):mesh(m), shader(s), textureID(0)
+    // Constructor que inicializa la malla, el shader y establece textureID a 0
+    Skybox::Skybox(MeshData m, ShaderProgram* s) : mesh(m), shader(s), textureID(0)
     {}
 
+    // Destructor que libera la textura del skybox
     Skybox::~Skybox()
     {
         glDeleteTextures(1, &textureID);
     }
 
+    // Establece el ID de la textura del skybox
     void Skybox::setTexture(GLuint textureID)
     {
         this->textureID = textureID;
     }
 
+    // Retorna el ID de la textura del skybox
     GLuint Skybox::getTextureID()
     {
         return textureID;
     }
 
+    // Renderiza el skybox usando las matrices de vista y proyección
     void Skybox::render(const glm::mat4& view, const glm::mat4& projection)
     {
         shader->use();
-        shader->setMat4("view", view);
+
+        // Quitar la traslación de la matriz de vista para que el skybox siga la cámara
+        glm::mat4 viewNoTranslation = glm::mat4(glm::mat3(view));
+        shader->setMat4("view", viewNoTranslation);
         shader->setMat4("projection", projection);
+
+        // Configurar el buffer de profundidad para que el skybox se dibuje correctamente
         glDepthFunc(GL_LEQUAL);
         glDepthMask(GL_FALSE);
 
+        // Bind de la textura del cubemap
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_CUBE_MAP, textureID);
 
+        // Deshabilitar el face culling para que se vean todas las caras
         glDisable(GL_CULL_FACE);
+
+        // Dibujar la malla del skybox
         mesh.render();
 
+        // Restaurar el estado del buffer de profundidad y face culling
         glDepthMask(GL_TRUE);
         glDepthFunc(GL_LESS);
+        glEnable(GL_CULL_FACE);
     }
 }
